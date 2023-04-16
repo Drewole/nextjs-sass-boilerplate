d5bde7d9e1c399900e2eee1b19eb57696a01b564
Drew Olsen <olsenj.andrew@gmail.com>
2023-04-16 18:31:53 -0500

    added hero module

--- components/modules/Hero.tsx
+++ components/modules/Hero.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from '@/styles/components/Hero.module.scss';
 import CommunityClinic from '../svg/CommunityClinic';
 import PublicHealth from '../svg/PublicHealth';
+import CreditsWrapper from './CreditsWrapper';
+import Link from 'next/link';
+import Button from '../partials/Button';
 
 type Props = {};
 
@@ -17,9 +20,22 @@ export default function Hero(props: Props) {
           the experience of care and promote health equity through addressing
           ethnic and racial disparities.
         </p>
+        <div className={styles.buttons}>
+          <Button link={{ href: '/about', text: 'Learn More' }}>
+            Here is the Text
+          </Button>
+        </div>
       </div>
       <div className={styles.graphic}>
-        <PublicHealth />
+        <CreditsWrapper
+          credits={
+            <Link target="_blank" href="https://storyset.com/education">
+              Education illustrations by Storyset
+            </Link>
+          }
+        >
+          <PublicHealth />
+        </CreditsWrapper>
       </div>
     </section>
   );
