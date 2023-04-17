4924dff87a164dccfdc5209d338cc8d97d004f11
Drew Olsen <olsenj.andrew@gmail.com>
2023-04-17 01:17:30 -0500

    removed graphic default width

--- components/svg/PublicHealth.tsx
+++ components/svg/PublicHealth.tsx
@@ -10,8 +10,6 @@ export default function CommunityClinic({ className, color }: Props) {
     <svg
       className={className}
       xmlns="http://www.w3.org/2000/svg"
-      width="750"
-      height="500"
       fill="none"
       viewBox="0 0 750 500"
     >
