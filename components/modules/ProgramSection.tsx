a5d58ed11a58d0d0fc96b37603d62bbbcfe06cb4
Drew Olsen <olsenj.andrew@gmail.com>
2023-05-03 23:12:37 -0500

    programs section

---
+++ components/modules/ProgramSection.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from '@/styles/components/ProgramSection.module.scss';
+
+type Props = {
+  className?: string;
+  mainTitle: string;
+  description?: string;
+  children?: React.ReactNode;
+};
+
+export function ProgramSection({
+  className = '',
+  mainTitle = 'Main Title',
+  description,
+  children,
+}: Props) {
+  return (
+    <div className={`${styles._} ${className}`}>
+      <h2>{mainTitle}</h2>
+      <p>{description}</p>
+      {children && <div className={styles.children}>{children}</div>}
+    </div>
+  );
+}
